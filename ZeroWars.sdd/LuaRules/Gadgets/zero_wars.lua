038b18d215a0b76d04beeb050628e94560cd533e
PetTurtle <ryhanillya@gmail.com>
2020-02-15 22:11:55 -0600

    Made built units unmovable

--- ZeroWars.sdd/LuaRules/Gadgets/zero_wars.lua
+++ ZeroWars.sdd/LuaRules/Gadgets/zero_wars.lua
@@ -13,6 +13,7 @@ function gadget:GetInfo()
     }
 end
 
+include("LuaRules/Configs/customcmds.h.lua")
 local Platform = VFS.Include("LuaRules/Gadgets/ZeroWData/Platform.lua")
 local IdleUnit = VFS.Include("LuaRules/Gadgets/ZeroWData/IdleUnit.lua")
 local Side = VFS.Include("LuaRules/Gadgets/ZeroWData/Side.lua")
@@ -26,6 +27,20 @@ local rightSide
 
 local updateTime = 60
 local idleUnits = {}
+local validCommands = {
+    CMD.FIRE_STATE,
+    CMD.MOVE_STATE,
+    CMD.REPEAT,
+    CMD.CLOAK,
+    CMD.ONOFF,
+    CMD.TRAJECTORY,
+    CMD_UNIT_AI,
+    CMD_AIR_STRAFE,
+    CMD_PUSH_PULL,
+    CMD_AP_FLY_STATE,
+    CMD_UNIT_BOMBER_DIVE_STATE,
+    CMD_AP_FLY_STATE,
+}
 
 ----- Initalizing Game -----
 
@@ -53,9 +68,9 @@ end
 local function InitializeLeftSide()
 
     -- Set platform data
-    local platformTop    = Platform.new(0, 1280, 1144)
+    local platformTop    = Platform.new(128, 1280, 1016)
     local platformCenter = Platform.new(1152, 1280, 0)
-    local platformBottom = Platform.new(2304, 1280, -1144)
+    local platformBottom = Platform.new(2184, 1280, -1016)
     local plats = {platformTop, platformCenter, platformBottom}
     
     for i = 1, #leftTeam.playerList do
@@ -76,9 +91,9 @@ end
 local function InitializeRightSide()
 
     -- Set platform data
-    local platformTop    = Platform.new(0, -1280,  1144)
+    local platformTop    = Platform.new(128, -1280,  1016)
     local platformCenter = Platform.new(1152, -1280,  0)
-    local platformBottom = Platform.new(2304, -1280, -1144)
+    local platformBottom = Platform.new(2184, -1280, -1016)
     local plats = {platformTop, platformCenter, platformBottom}
 
     for i = 1, #rightTeam.playerList do
@@ -97,55 +112,52 @@ end
 
 -- Sets left side data on first frame
 local function CreateLeftSide()
-    leftSide.baseId = Spring.CreateUnit("baseturret", 2303, 10000, 1530, "e", leftTeam.nullAI)
+    leftSide.baseId = Spring.CreateUnit("baseturret", 2496, 10000, 1530, "e", leftTeam.nullAI)
     leftSide.turretId= Spring.CreateUnit("centerturret", 3264, 10000, 1530, "e", leftTeam.nullAI)
     Spring.SetUnitBlocking(leftSide.baseId, false)
     Spring.SetUnitBlocking(leftSide.turretId, false)
 
     for i = 1, #leftSide.plats do
         for t = 1, #leftSide.plats[i].players do
-            Spring.CreateUnit("basiccon", leftSide.plats[i].rect.x1, 10000, leftSide.plats[i].rect.y1 + 350, "e", leftSide.plats[i].players[t])
+            Spring.CreateUnit("basiccon", leftSide.plats[i].rect.x1 + 192.5, 10000, leftSide.plats[i].rect.y1 + 350, "e", leftSide.plats[i].players[t])
             local units = Spring.GetTeamUnits(leftSide.plats[i].players[t])
             Spring.SetUnitPosition(units[1], 1855, 1537)
-            local aa = Spring.CreateUnit("superaaturret", leftSide.plats[i].rect.x1 + 364, 10000, leftSide.plats[i].rect.y1 + 380, "e", leftTeam.nullAI)
         end
     end
 
-    Spring.CreateUnit("staticrearm", 1480, 10000, 1226, "e", leftTeam.nullAI)
-    Spring.CreateUnit("staticrearm", 1480, 10000, 1386, "e", leftTeam.nullAI)
-    Spring.CreateUnit("staticrearm", 1480, 10000, 1529, "e", leftTeam.nullAI)
-    Spring.CreateUnit("staticrearm", 1480, 10000, 1703, "e", leftTeam.nullAI)
-    Spring.CreateUnit("staticrearm", 1480, 10000, 1848, "e", leftTeam.nullAI)
+    Spring.CreateUnit("staticrearm", 1550, 10000, 1226, "e", leftTeam.nullAI)
+    Spring.CreateUnit("staticrearm", 1550, 10000, 1386, "e", leftTeam.nullAI)
+    Spring.CreateUnit("staticrearm", 1550, 10000, 1529, "e", leftTeam.nullAI)
+    Spring.CreateUnit("staticrearm", 1550, 10000, 1703, "e", leftTeam.nullAI)
+    Spring.CreateUnit("staticrearm", 1550, 10000, 1848, "e", leftTeam.nullAI)
 
     Spring.SetTeamResource(leftTeam.nullAI, "metal", 0)
     for i = 1, #leftTeam.playerList do
         Spring.SetTeamResource(leftTeam.playerList[i], "metal", 0)
     end
 
-    Spring.CreateUnit("staticstorage", 0, 10000, 0, "n", leftTeam.nullAI)
     local nullAICom = Spring.GetUnitsInRectangle(3968, 1152, 4224, 1920, leftTeam.nullAI)
     Spring.DestroyUnit(nullAICom[1], false, true)
 end
 
 -- Sets right side data on first frame
 local function CreateRightSide()
-    rightSide.baseId = Spring.CreateUnit("baseturret", 5888, 10000, 1530, "w", rightTeam.nullAI)
+    rightSide.baseId = Spring.CreateUnit("baseturret", 5696, 10000, 1530, "w", rightTeam.nullAI)
     rightSide.turretId = Spring.CreateUnit("centerturret", 4930, 10000, 1530, "w", rightTeam.nullAI)
     Spring.SetUnitBlocking(rightSide.baseId, false)
     Spring.SetUnitBlocking(rightSide.turretId, false)
 
-    Spring.CreateUnit("staticrearm", 6711, 10000, 1226, "w", rightTeam.nullAI)
-    Spring.CreateUnit("staticrearm", 6711, 10000, 1386, "w", rightTeam.nullAI)
-    Spring.CreateUnit("staticrearm", 6711, 10000, 1529, "w", rightTeam.nullAI)
-    Spring.CreateUnit("staticrearm", 6711, 10000, 1703, "w", rightTeam.nullAI)
-    Spring.CreateUnit("staticrearm", 6711, 10000, 1848, "w", rightTeam.nullAI)
+    Spring.CreateUnit("staticrearm", 6641, 10000, 1226, "w", rightTeam.nullAI)
+    Spring.CreateUnit("staticrearm", 6641, 10000, 1386, "w", rightTeam.nullAI)
+    Spring.CreateUnit("staticrearm", 6641, 10000, 1529, "w", rightTeam.nullAI)
+    Spring.CreateUnit("staticrearm", 6641, 10000, 1703, "w", rightTeam.nullAI)
+    Spring.CreateUnit("staticrearm", 6641, 10000, 1848, "w", rightTeam.nullAI)
 
     for i = 1, #rightSide.plats do
         for t = 1, #rightSide.plats[i].players do
-            Spring.CreateUnit("basiccon", rightSide.plats[i].rect.x2, 10000, rightSide.plats[i].rect.y2 - 350, "e", rightSide.plats[i].players[t])
+            Spring.CreateUnit("basiccon", rightSide.plats[i].rect.x2 - 192.5, 10000, rightSide.plats[i].rect.y2 - 350, "e", rightSide.plats[i].players[t])
             local units = Spring.GetTeamUnits(rightSide.plats[i].players[t])
             Spring.SetUnitPosition(units[1], 6336, 1537)
-            local aa = Spring.CreateUnit("superaaturret", rightSide.plats[i].rect.x2 - 364, 10000, rightSide.plats[i].rect.y1 + 370, "w", rightTeam.nullAI)
         end
     end
 
@@ -154,7 +166,6 @@ local function CreateRightSide()
         Spring.SetTeamResource(rightTeam.playerList[i], "metal", 0)
     end
 
-    Spring.CreateUnit("staticstorage", 8192, 10000, 0, "n", rightTeam.nullAI)
     local nullAICom = Spring.GetUnitsInRectangle(3968, 1152, 4224, 1920, rightTeam.nullAI)
     Spring.DestroyUnit(nullAICom[1], false, true)
 end
@@ -191,13 +202,6 @@ end
 
 function gadget:GameFrame(f)
     if f == 1 then
-        Spring.CreateUnit("superaaturret", 2940, 10000, 4, "s", Spring.GetGaiaTeamID())
-        Spring.CreateUnit("superaaturret", 4092, 10000, 4, "s", Spring.GetGaiaTeamID())
-        Spring.CreateUnit("superaaturret", 5244, 10000, 4, "s", Spring.GetGaiaTeamID())
-        Spring.CreateUnit("superaaturret", 2940, 10000, 3056, "n", Spring.GetGaiaTeamID())
-        Spring.CreateUnit("superaaturret", 4092, 10000, 3056, "n", Spring.GetGaiaTeamID())
-        Spring.CreateUnit("superaaturret", 5244, 10000, 3056, "n", Spring.GetGaiaTeamID())
-
         CreateLeftSide()
         CreateRightSide()
         dataSet = true
@@ -248,18 +252,20 @@ function gadget:UnitIdle(unitID, unitDefID, unitTeam)
     end
 end
 
-function gadget:AllowUnitTransfer(unitID, unitDefID, oldTeam, newTeam, capture)
-    if newTeam == leftTeam.nullAI or newTeam == rightTeam.nullAI then
-        return true
+function SetGroundUnbuildable(x, y, sizex, sizey)
+    --SetGroundUnbuildable(x/16, z/16, xsize, zsize)
+    for iX = x-sizex, x+sizex - 1 do
+        for iY = y-sizey, y+sizey - 1 do
+            Spring.SetSquareBuildingMask(iX, iY, 2)
+        end
     end
-    return false
 end
 
 -- Don't allow factories in center
 function gadget:AllowUnitCreation(unitDefID, builderID, builderTeam, x, y, z, facing)
     local ud = UnitDefs[unitDefID]
     if dataSet then
-        if ud.isBuilding then
+        if ud.isBuilding and x and x > 374 and x < 7817 then
             if ud.isFactory or ud.isStaticBuilder then
                 return false
             end
@@ -267,8 +273,40 @@ function gadget:AllowUnitCreation(unitDefID, builderID, builderTeam, x, y, z, fa
                 return false
             end
         end
+        
+        xsize = ud.xsize and tonumber(ud.xsize) / 4 or 1
+        zsize = ud.zsize and tonumber(ud.zsize) / 4 or 1
+        local gridSize = 16
+        if Spring.GetGroundBlocked(x - xsize * gridSize, z - zsize * gridSize, x + (xsize-1) * gridSize, z + (zsize-1) * gridSize) ~= false then
+            return false
+        end
     elseif ud.isTransport then
         return false
     end
     return true
 end
+
+function gadget:AllowCommand(unitID, unitDefID, unitTeam, cmdID, cmdParams, cmdOptions, cmdTag, synced)
+    local x, y, z = Spring.GetUnitPosition(unitID)
+    local ud = UnitDefs[unitDefID]
+
+    if not x or x < 760 or x > 8192 - 760 then
+        if ud.isBuilder then
+            return true
+        end
+        for i = 1, #validCommands do
+            if (validCommands[i] == cmdID) then
+                return true
+            end
+        end
+        return false
+    end
+    return true
+end
+
+-- function gadget:AllowUnitTransfer(unitID, unitDefID, oldTeam, newTeam, capture)
+--     if newTeam == leftTeam.nullAI or newTeam == rightTeam.nullAI then
+--         return true
+--     end
+--     return false
+-- end
