996432c20cd6c1996b1d49ddf4a4adf5f800afff
PetTurtle <ryhanillya@gmail.com>
2020-02-15 22:10:24 -0600

    New map

---
+++ terrain_textures/ZeroWarsTileSet.tsx
@@ -0,0 +1,55 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.2" name="ZeroWarsTileSet" tilewidth="128" tileheight="128" tilecount="17" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="64" height="64" source="../../UsedTextures/01_diffuse.png"/>
+ </tile>
+ <tile id="1">
+  <image width="128" height="128" source="../../UsedTextures/02_diffuse.png"/>
+ </tile>
+ <tile id="2">
+  <image width="128" height="128" source="../../UsedTextures/10_diffuse.png"/>
+ </tile>
+ <tile id="3">
+  <image width="128" height="128" source="../../UsedTextures/31_diffuse.png"/>
+ </tile>
+ <tile id="4">
+  <image width="128" height="128" source="../../UsedTextures/39_diffuse.png"/>
+ </tile>
+ <tile id="5">
+  <image width="128" height="128" source="../../UsedTextures/40_diffuse.png"/>
+ </tile>
+ <tile id="6">
+  <image width="128" height="128" source="../../UsedTextures/41_diffuse.png"/>
+ </tile>
+ <tile id="7">
+  <image width="128" height="128" source="../../UsedTextures/48_diffuse.png"/>
+ </tile>
+ <tile id="8">
+  <image width="128" height="128" source="../../UsedTextures/134_diffuse.png"/>
+ </tile>
+ <tile id="9">
+  <image width="128" height="128" source="../../UsedTextures/135_diffuse.png"/>
+ </tile>
+ <tile id="10">
+  <image width="128" height="128" source="../../UsedTextures/136_diffuse.png"/>
+ </tile>
+ <tile id="11">
+  <image width="128" height="128" source="../../UsedTextures/137_diffuse.png"/>
+ </tile>
+ <tile id="12">
+  <image width="128" height="128" source="../../UsedTextures/138_diffuse.png"/>
+ </tile>
+ <tile id="13">
+  <image width="128" height="128" source="../../UsedTextures/182_diffuse.png"/>
+ </tile>
+ <tile id="14">
+  <image width="128" height="128" source="../../UsedTextures/244_diffuse.png"/>
+ </tile>
+ <tile id="15">
+  <image width="128" height="128" source="../../UsedTextures/250_diffuse.png"/>
+ </tile>
+ <tile id="16">
+  <image width="128" height="128" source="../../UsedTextures/254_diffuse.png"/>
+ </tile>
+</tileset>
