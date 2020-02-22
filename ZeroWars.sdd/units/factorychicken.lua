05783a9a0a00bb78d04212b106913ce51ea165f7
PetTurtle <ryhanillya@gmail.com>
2020-02-21 19:59:58 -0600

    added chicken factory

---
+++ ZeroWars.sdd/units/factorychicken.lua
@@ -0,0 +1,86 @@
+unitDef = {
+    unitname                      = [[factorychicken]],
+    name                          = [[Chicken Factory]],
+    description                   = [[Builds at 10 m/s]],
+    acceleration                  = 0,
+    brakeRate                     = 1.5,
+    buildCostMetal                = 400,
+    buildDistance                 = 800,
+    builder                       = true,
+    buildingGroundDecalDecaySpeed = 30,
+    buildingGroundDecalSizeX      = 13,
+    buildingGroundDecalSizeY      = 13,
+    buildingGroundDecalType       = [[factorycloak_aoplane.dds]],
+  
+    buildoptions                  = {
+        [[chicken]],
+        [[chicken_leaper]],
+        [[chickens]],
+        [[chicken_dodo]],
+        [[chicken_pigeon]],
+    },
+  
+    buildPic                      = [[chickenbroodqueen.png]],
+    canGuard                      = true,
+    canMove                       = false,
+    canPatrol                     = true,
+    cantBeTransported             = true,
+    category                      = [[FLOAT UNARMED]],
+    collisionVolumeOffsets        = [[0 0 0]],
+    collisionVolumeScales         = [[70 70 70]],
+    collisionVolumeType           = [[ellipsoid]],
+    corpse                        = [[DEAD]],
+  
+    customParams                  = {
+      morphto = [[factorychickenadv]],
+      morphcost = 800,
+    },
+  
+    explodeAs                     = [[ESTOR_BUILDINGEX]],
+    floater                       = true,
+    footprintX                    = 7,
+    footprintZ                    = 10,
+    iconType                      = [[fackbot]],
+    idleAutoHeal                  = 5,
+    idleTime                      = 1800,
+    levelGround                   = false,
+    maneuverleashlength           = [[380]],
+    maxDamage                     = 2000,
+    maxSlope                      = 15,
+    maxVelocity                   = 0,
+    minCloakDistance              = 150,
+    movementClass                 = [[KBOT4]],
+    noAutoFire                    = false,
+    objectName                    = [[chickenbroodqueen.s3o]],
+    script                        = [[customFactory.lua]],
+    selfDestructAs                = [[LARGE_BUILDINGEX]],
+    showNanoSpray                 = true,
+    sightDistance                 = 380,
+    turnRate                      = 1,
+    upright                       = true,
+    useBuildingGroundDecal        = true,
+    workerTime                    = 10,
+  
+    featureDefs                   = {
+  
+      DEAD = {
+        blocking         = false,
+        featureDead      = [[HEAP]],
+        footprintX       = 7,
+        footprintZ       = 7,
+        object           = [[cremfactorywreck.s3o]],
+      },
+  
+  
+      HEAP = {
+        blocking         = false,
+        footprintX       = 7,
+        footprintZ       = 7,
+        object           = [[debris4x4b.s3o]],
+      },
+  
+    },
+  
+  }
+  
+  return lowerkeys({factorychicken = unitDef})
---
+++ ZeroWars.sdd/units/factorychickenadv.lua
@@ -0,0 +1,88 @@
+unitDef = {
+    unitname                      = [[factorychickenadv]],
+    name                          = [[Advanced Chicken Factory]],
+    description                   = [[Builds at 10 m/s]],
+    acceleration                  = 0,
+    brakeRate                     = 1.5,
+    buildCostMetal                = 400,
+    buildDistance                 = 800,
+    builder                       = true,
+    buildingGroundDecalDecaySpeed = 30,
+    buildingGroundDecalSizeX      = 8,
+    buildingGroundDecalSizeY      = 8,
+    buildingGroundDecalType       = [[factorycloak_aoplane.dds]],
+  
+    buildoptions                  = {
+        [[chicken]],
+        [[chicken_leaper]],
+        [[chickens]],
+        [[chicken_dodo]],
+        [[chicken_pigeon]],
+        [[chickena]],
+        [[chickenc]],
+        [[chicken_blimpy]],
+        [[chicken_dragon]],
+    },
+  
+    buildPic                      = [[chickenflyerqueen.png]],
+    canGuard                      = true,
+    canMove                       = false,
+    canPatrol                     = true,
+    cantBeTransported             = true,
+    category                      = [[FLOAT UNARMED]],
+    collisionVolumeOffsets        = [[0 0 0]],
+    collisionVolumeScales         = [[70 70 70]],
+    collisionVolumeType           = [[ellipsoid]],
+    corpse                        = [[DEAD]],
+  
+    customParams                  = {
+    },
+  
+    explodeAs                     = [[ESTOR_BUILDINGEX]],
+    floater                       = true,
+    footprintX                    = 8,
+    footprintZ                    = 8,
+    iconType                      = [[fackbot]],
+    idleAutoHeal                  = 5,
+    idleTime                      = 1800,
+    levelGround                   = false,
+    maneuverleashlength           = [[380]],
+    maxDamage                     = 2000,
+    maxSlope                      = 15,
+    maxVelocity                   = 0,
+    minCloakDistance              = 150,
+    movementClass                 = [[KBOT4]],
+    noAutoFire                    = false,
+    objectName                    = [[chickenflyerqueen.s3o]],
+    script                        = [[customFactoryChickenQ.lua]],
+    selfDestructAs                = [[SMALL_UNITEX]],
+    showNanoSpray                 = true,
+    sightDistance                 = 380,
+    turnRate                      = 1,
+    upright                       = true,
+    useBuildingGroundDecal        = true,
+    workerTime                    = 10,
+  
+    featureDefs                   = {
+  
+      DEAD = {
+        blocking         = false,
+        featureDead      = [[HEAP]],
+        footprintX       = 7,
+        footprintZ       = 7,
+        object           = [[cremfactorywreck.s3o]],
+      },
+  
+  
+      HEAP = {
+        blocking         = false,
+        footprintX       = 7,
+        footprintZ       = 7,
+        object           = [[debris4x4b.s3o]],
+      },
+  
+    },
+  
+  }
+  
+  return lowerkeys({factorychickenadv = unitDef})
