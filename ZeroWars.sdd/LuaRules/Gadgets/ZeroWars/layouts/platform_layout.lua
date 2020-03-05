d5a420271107ffb17d558b54a802eea0d1f8d20d
pt <ryhanillya@gmail.com>
2020-03-04 19:52:56 -0600

    Updated platform_layout

--- ZeroWars.sdd/LuaRules/Gadgets/ZeroWars/layouts/platform_layout.lua
+++ ZeroWars.sdd/LuaRules/Gadgets/ZeroWars/layouts/platform_layout.lua
@@ -19,17 +19,17 @@ function Platform_Layout.new(side)
     local platRightOffset = mapWidth - platWidth - 380
 
     if side == "left" then
-        table.insert(platforms, Platform.new(Rect.new(platLeftOffset, 128, platWidth, platHeight), 1280,  1016))
-        table.insert(platforms, Platform.new(Rect.new(platLeftOffset, 1152, platWidth, platHeight), 1280,  0))
-        table.insert(platforms, Platform.new(Rect.new(platLeftOffset, 2184, platWidth, platHeight), 1280, -1016))
+        table.insert(platforms, Platform:new(Rect:new(platLeftOffset, 128, platWidth, platHeight), 1280,  1016))
+        table.insert(platforms, Platform:new(Rect:new(platLeftOffset, 1152, platWidth, platHeight), 1280,  0))
+        table.insert(platforms, Platform:new(Rect:new(platLeftOffset, 2184, platWidth, platHeight), 1280, -1016))
 
-        deployPlatform = Rect.new(1664, 1152, platWidth, platHeight)
+        deployPlatform = Rect:new(1664, 1152, platWidth, platHeight)
     elseif side == "right" then
-        table.insert(platforms, Platform.new(Rect.new(platRightOffset, 128, platWidth, platHeight), -1280,  1016))
-        table.insert(platforms, Platform.new(Rect.new(platRightOffset, 1152, platWidth, platHeight), -1280,  0))
-        table.insert(platforms, Platform.new(Rect.new(platRightOffset, 2184, platWidth, platHeight), -1280, -1016))
+        table.insert(platforms, Platform:new(Rect:new(platRightOffset, 128, platWidth, platHeight), -1280,  1016))
+        table.insert(platforms, Platform:new(Rect:new(platRightOffset, 1152, platWidth, platHeight), -1280,  0))
+        table.insert(platforms, Platform:new(Rect:new(platRightOffset, 2184, platWidth, platHeight), -1280, -1016))
 
-        deployPlatform = Rect.new(6144, 1152, platWidth, platHeight)
+        deployPlatform = Rect:new(6144, 1152, platWidth, platHeight)
     end
     local platform_layout = {
         platforms = platforms,
