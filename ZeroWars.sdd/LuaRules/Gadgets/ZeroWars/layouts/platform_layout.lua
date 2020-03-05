948ae55398e34b86ed4314d520edccc129f01897
pt <ryhanillya@gmail.com>
2020-03-04 21:04:38 -0600

    Platform layout perfectly matches map

--- ZeroWars.sdd/LuaRules/Gadgets/ZeroWars/layouts/platform_layout.lua
+++ ZeroWars.sdd/LuaRules/Gadgets/ZeroWars/layouts/platform_layout.lua
@@ -13,21 +13,21 @@ function Platform_Layout.new(side)
     local platforms = {}
     local deployPlatform
 
-    local platWidth = 380
-    local platHeight = 760
-    local platLeftOffset = 380
-    local platRightOffset = mapWidth - platWidth - 380
+    local platWidth = 368
+    local platHeight = 752
+    local platLeftOffset = 384
+    local platRightOffset = mapWidth - platWidth - platLeftOffset - 16
 
     if side == "left" then
         table.insert(platforms, Platform:new(Rect:new(platLeftOffset, 128, platWidth, platHeight), 1280,  1016))
         table.insert(platforms, Platform:new(Rect:new(platLeftOffset, 1152, platWidth, platHeight), 1280,  0))
-        table.insert(platforms, Platform:new(Rect:new(platLeftOffset, 2184, platWidth, platHeight), 1280, -1016))
+        table.insert(platforms, Platform:new(Rect:new(platLeftOffset, 2176, platWidth, platHeight), 1280, -1016))
 
         deployPlatform = Rect:new(1664, 1152, platWidth, platHeight)
     elseif side == "right" then
         table.insert(platforms, Platform:new(Rect:new(platRightOffset, 128, platWidth, platHeight), -1280,  1016))
         table.insert(platforms, Platform:new(Rect:new(platRightOffset, 1152, platWidth, platHeight), -1280,  0))
-        table.insert(platforms, Platform:new(Rect:new(platRightOffset, 2184, platWidth, platHeight), -1280, -1016))
+        table.insert(platforms, Platform:new(Rect:new(platRightOffset, 2176, platWidth, platHeight), -1280, -1016))
 
         deployPlatform = Rect:new(6144, 1152, platWidth, platHeight)
     end
