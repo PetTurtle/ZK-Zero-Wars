9eb6aa430fdbff69d85b2b76cf2a643a2c7c189d
pt <ryhanillya@gmail.com>
2020-03-05 19:57:04 -0600

    Added unitdefs_post for future

---
+++ ZeroWars.sdd/gamedata/unitdefs_post.lua
@@ -0,0 +1,12 @@
+-- Set chicken cost
+-- for name, ud in pairs(UnitDefs) do
+--     if (ud.unitname:sub(1,7) == "chicken") then
+--         ud.buildcostmetal = ud.buildtime
+--         ud.buildcostenergy = ud.buildtime
+--     end
+--     if (ud.unitname == "factorychicken") then
+--         ud.buildoptions = {
+--             [[factorychicken]],
+--         }
+--     end
+-- end
