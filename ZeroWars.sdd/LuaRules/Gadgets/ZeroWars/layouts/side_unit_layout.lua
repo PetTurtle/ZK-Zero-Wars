08cc8013ad28f1f2f0cc6dace1db1e164bac7f19
pt <ryhanillya@gmail.com>
2020-03-14 22:03:24 -0500

    added option for unselectable spawned buildings

--- ZeroWars.sdd/LuaRules/Gadgets/ZeroWars/layouts/side_unit_layout.lua
+++ ZeroWars.sdd/LuaRules/Gadgets/ZeroWars/layouts/side_unit_layout.lua
@@ -6,21 +6,21 @@ function Side_Unit_Layout.new(side)
     local units = {}
 
     if side == "left" then
-        table.insert(units, {unitName = "baseturret", x = 2496, z = 1530, dir = "e"})
-        table.insert(units, {unitName = "centerturret", x = 3264, z = 1530, dir = "e"})
-        table.insert(units, {unitName = "staticrearm", x = 1550, z = 1226, dir = "e"})
-        table.insert(units, {unitName = "staticrearm", x = 1550, z = 1386, dir = "e"})
-        table.insert(units, {unitName = "staticrearm", x = 1550, z = 1529, dir = "e"})
-        table.insert(units, {unitName = "staticrearm", x = 1550, z = 1703, dir = "e"})
-        table.insert(units, {unitName = "staticrearm", x = 1550, z = 1848, dir = "e"})
+        table.insert(units, {unitName = "baseturret", x = 2496, z = 1530, dir = "e", noSelectable = false,})
+        table.insert(units, {unitName = "centerturret", x = 3264, z = 1530, dir = "e", noSelectable = false,})
+        table.insert(units, {unitName = "staticrearm", x = 1550, z = 1226, dir = "e", noSelectable = true,})
+        table.insert(units, {unitName = "staticrearm", x = 1550, z = 1386, dir = "e", noSelectable = true,})
+        table.insert(units, {unitName = "staticrearm", x = 1550, z = 1529, dir = "e", noSelectable = true,})
+        table.insert(units, {unitName = "staticrearm", x = 1550, z = 1703, dir = "e", noSelectable = true,})
+        table.insert(units, {unitName = "staticrearm", x = 1550, z = 1848, dir = "e", noSelectable = true,})
     elseif side == "right" then
-        table.insert(units, {unitName = "baseturret", x = 5696, z = 1530, dir = "w"})
-        table.insert(units, {unitName = "centerturret", x = 4930, z = 1530, dir = "w"})
-        table.insert(units, {unitName = "staticrearm", x = 6641, z = 1226, dir = "w"})
-        table.insert(units, {unitName = "staticrearm", x = 6641, z = 1386, dir = "w"})
-        table.insert(units, {unitName = "staticrearm", x = 6641, z = 1529, dir = "w"})
-        table.insert(units, {unitName = "staticrearm", x = 6641, z = 1703, dir = "w"})
-        table.insert(units, {unitName = "staticrearm", x = 6641, z = 1848, dir = "w"})
+        table.insert(units, {unitName = "baseturret", x = 5696, z = 1530, dir = "w", noSelectable = false,})
+        table.insert(units, {unitName = "centerturret", x = 4930, z = 1530, dir = "w", noSelectable = false,})
+        table.insert(units, {unitName = "staticrearm", x = 6641, z = 1226, dir = "w", noSelectable = true,})
+        table.insert(units, {unitName = "staticrearm", x = 6641, z = 1386, dir = "w", noSelectable = true,})
+        table.insert(units, {unitName = "staticrearm", x = 6641, z = 1529, dir = "w", noSelectable = true,})
+        table.insert(units, {unitName = "staticrearm", x = 6641, z = 1703, dir = "w", noSelectable = true,})
+        table.insert(units, {unitName = "staticrearm", x = 6641, z = 1848, dir = "w", noSelectable = true,})
     end
 
     return units
