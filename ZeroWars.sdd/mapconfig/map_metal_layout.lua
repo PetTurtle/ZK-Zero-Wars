663d7094428ca709eb2cfc3821370a22b8f6c3ed
pt <ryhanillya@gmail.com>
2020-03-14 21:59:20 -0500

    metal map does not skip nullAI

--- ZeroWars.sdd/mapconfig/map_metal_layout.lua
+++ ZeroWars.sdd/mapconfig/map_metal_layout.lua
@@ -1,20 +1,6 @@
-function GetPlayerList(allyID)
-    local playerList = Spring.GetTeamList(allyID)
-
-    -- remove nullAI from player list
-    for i = #playerList, 1, -1 do
-        local luaAI = Spring.GetTeamLuaAI(playerList[i])
-        if luaAI and string.find(string.lower(luaAI), "ai") then
-            table.remove(playerList, i)
-        end
-    end
-
-	return playerList
-end
-
 local allyTeamList = Spring.GetAllyTeamList()
-local leftTeam = GetPlayerList(allyTeamList[1])
-local rightTeam = GetPlayerList(allyTeamList[2])
+local leftTeam = Spring.GetTeamList(allyTeamList[1])
+local rightTeam = Spring.GetTeamList(allyTeamList[2])
 
 local totalPlayers = #leftTeam + #rightTeam
 
