210ab3cf2f75eda4ae80158cf3bdeb6253e53c53
pt <ryhanillya@gmail.com>
2020-03-15 14:32:35 -0500

    added chicken unit_tweaks

--- ZeroWars.sdd/gamedata/unitdefs_post.lua
+++ ZeroWars.sdd/gamedata/unitdefs_post.lua
@@ -1,5 +1,7 @@
--- Set chicken cost
+local unitTweaks = VFS.Include("gamedata/unit_tweaks.lua")
+
 for name, ud in pairs(UnitDefs) do
+	-- Add Chicken Factory
     if (ud.unitname:sub(1,7) == "chicken") then
         ud.buildcostmetal = ud.buildtime
         ud.buildcostenergy = ud.buildtime
@@ -8,6 +10,7 @@ for name, ud in pairs(UnitDefs) do
 		ud.buildoptions[#ud.buildoptions + 1] = [[factorychicken]]
 	end
 	
+	-- Removed Friendly Fire Collisions
 	ud.avoidFriendly = false
 	ud.collideFriendly = false
 	ud.collideFirebase = false
@@ -21,6 +24,17 @@ for name, ud in pairs(UnitDefs) do
 			else
 				wd.customparams.nofriendlyfire = 1
 			end
+		end
+	end
+end
+
+-- apply unitTweaks
+if unitTweaks and type(unitTweaks) == "table" then
+	Spring.Echo("Loading custom units tweaks for zero-wars")
+	for name, ud in pairs(UnitDefs) do
+		if unitTweaks[name] then
+			Spring.Echo("Loading custom units tweaks for " .. name)
+			Spring.Utilities.OverwriteTableInplace(ud, lowerkeys(unitTweaks[name]), true)
 		end
 	end
 end
