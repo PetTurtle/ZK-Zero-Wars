9c3d6f07c26e9a554f5b1e896325bad8be63a12a
pt <ryhanillya@gmail.com>
2020-03-14 21:58:46 -0500

    Fixed unit_defs_post

--- ZeroWars.sdd/gamedata/unitdefs_post.lua
+++ ZeroWars.sdd/gamedata/unitdefs_post.lua
@@ -1,19 +1,17 @@
 -- Set chicken cost
--- for name, ud in pairs(UnitDefs) do
---     if (ud.unitname:sub(1,7) == "chicken") then
---         ud.buildcostmetal = ud.buildtime
---         ud.buildcostenergy = ud.buildtime
---     end
---     if (ud.unitname == "factorychicken") then
---         ud.buildoptions = {
---             [[factorychicken]],
---         }
---     end
--- end
+for name, ud in pairs(UnitDefs) do
+    if (ud.unitname:sub(1,7) == "chicken") then
+        ud.buildcostmetal = ud.buildtime
+        ud.buildcostenergy = ud.buildtime
+    end
+	if (ud.unitname == "basiccon") then
+		ud.buildoptions[#ud.buildoptions + 1] = [[factorychicken]]
+    end
+end
 
-for _, ud in pairs(UnitDefs) do
-	if ud.weaponDefs then
-	    for _, wd in  pairs(ud.weaponDefs) do
+for name, ud in pairs(UnitDefs) do
+	if ud.weapondefs then
+		for _, wd in pairs(ud.weapondefs) do
 			if not wd.customparams then
 				wd.customparams = { nofriendlyfire=1 }
 			else
