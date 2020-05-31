d9b74ab4320a8e86201def31ee9e4f6f669bc4c9
UndoBlaze <ryhanillya@gmail.com>
2020-05-31 01:07:48 -0500

    Tweaked HeroPuppy

--- ZeroWars.sdd/units/heropuppy.lua
+++ ZeroWars.sdd/units/heropuppy.lua
@@ -132,7 +132,7 @@ return {
                 flightTime = 12,
                 fireStarter = 100,
                 impulseFactor = 0,
-                damage = {default = 200},
+                damage = {default = 500},
                 edgeEffectiveness = 0.5,
                 explosionGenerator = [[custom:MEDMISSILE_EXPLOSION]],
                 interceptedByShieldType = 1,
