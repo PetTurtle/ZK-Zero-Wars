55e980d019a2cf9b21dcab4bfd141c43e210a5a9
PetTurtle <ryhanillya@gmail.com>
2020-06-11 15:10:50 -0500

    added unit_energy

--- ZeroWars.sdd/gamedata/unitdefs_map.lua
+++ ZeroWars.sdd/gamedata/unitdefs_map.lua
@@ -1,4 +1,5 @@
 local unitTweaks = VFS.Include("gamedata/unit_tweaks.lua")
+local unitEnergy = VFS.Include("gamedata/unit_energy.lua")
 
 UnitDefs["factoryship"] = UnitDefs["factorychicken"]
 
@@ -9,6 +10,17 @@ if unitTweaks and type(unitTweaks) == "table" then
 		if unitTweaks[name] then
 			Spring.Echo("Loading custom units tweaks for " .. name)
 			Spring.Utilities.OverwriteTableInplace(ud, lowerkeys(unitTweaks[name]), true)
+		end
+	end
+end
+
+-- apply unitEnergy
+if unitEnergy and type(unitEnergy) == "table" then
+	Spring.Echo("Loading custom units energy for zero-wars")
+	for name, ud in pairs(UnitDefs) do
+		if unitEnergy[name] then
+			Spring.Echo("Loading custom units energy for " .. name)
+			Spring.Utilities.OverwriteTableInplace(ud, lowerkeys(unitEnergy[name]), true)
 		end
 	end
 end
