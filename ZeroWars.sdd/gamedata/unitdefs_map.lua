f1fbb962e0ed10b0a85e1fda7faee9f9f5c138ae
PetTurtle <ryhanillya@gmail.com>
2020-06-14 22:32:38 -0700

    fixed chicken cost, removed friendly fire and collisions

--- ZeroWars.sdd/gamedata/unitdefs_map.lua
+++ ZeroWars.sdd/gamedata/unitdefs_map.lua
@@ -3,6 +3,35 @@ local unitEnergy = VFS.Include("gamedata/unit_energy.lua")
 
 UnitDefs["factoryship"] = UnitDefs["factorychicken"]
 
+for name, ud in pairs(UnitDefs) do
+	-- set chicken cost
+    if (ud.unitname:sub(1,7) == "chicken") then
+        ud.buildcostmetal = ud.buildtime
+        ud.buildcostenergy = ud.buildtime
+	end
+	
+	-- removed friendly fire collisions
+	ud.avoidFriendly = false
+	ud.collideFriendly = false
+	ud.collideFirebase = false
+end
+
+-- remove friendly fire damage
+for name, ud in pairs(UnitDefs) do
+	if ud.weapondefs then
+		if (ud.unitname ~= "chicken_dodo") then
+			for _, wd in pairs(ud.weapondefs) do
+				if not wd.customparams then
+					wd.customparams = { nofriendlyfire=1 }
+				else
+					wd.customparams.nofriendlyfire = 1
+				end
+			end
+		end
+	end
+end
+
+
 -- apply unitTweaks
 if unitTweaks and type(unitTweaks) == "table" then
 	Spring.Echo("Loading custom units tweaks for zero-wars")
