36ba3f5850e0c9849339d2faa83f2eeea7e52e4f
PetTurtle <ryhanillya@gmail.com>
2020-06-16 00:24:44 -0700

    start with 600 metal

--- ZeroWars.sdd/luarules/gadgets/zerowars.lua
+++ ZeroWars.sdd/luarules/gadgets/zerowars.lua
@@ -68,7 +68,7 @@ end
 
 function gadget:GameStart()
     local builders = map:replaceStartUnit("builder")
-    map:setMetalStorage(300)
+    map:setMetalStorage(600)
 
     for _, builderID in pairs(builders) do
         local allyTeamID = Spring.GetUnitAllyTeam(builderID)
