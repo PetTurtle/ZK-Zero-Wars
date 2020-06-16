20c7e0f6f204a34f55c0418191ac0f56f9d6d309
PetTurtle <ryhanillya@gmail.com>
2020-06-15 23:35:24 -0700

    added unit_death_event.lua

---
+++ ZeroWars.sdd/luarules/gadgets/unit_death_event.lua
@@ -0,0 +1,40 @@
+function gadget:GetInfo()
+    return {
+        name = "Death Event",
+        desc = "call function when unit dies",
+        author = "petturtle",
+        date = "2020",
+        license = "GNU GPL, v2 or later",
+        layer = 0,
+        enabled = true,
+    }
+end
+
+if not gadgetHandler:IsSyncedCode() then
+    return false
+end
+
+-----------------------------------------
+-----------------------------------------
+
+local _events = {}
+
+function AddOnDeathEvent(unitID, event)
+    _events[unitID] = event
+end
+
+function gadget:Initialize()
+    if Game.modShortName ~= "ZK" then
+        gadgetHandler:RemoveGadget()
+        return
+    end
+
+    GG.AddOnDeathEvent = AddOnDeathEvent
+end
+
+function gadget:UnitDestroyed(unitID)
+    if _events[unitID] then
+        _events[unitID]()
+        table.remove(_events, unitID)
+    end
+end
