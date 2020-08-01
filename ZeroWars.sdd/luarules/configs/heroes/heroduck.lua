903dc8fa0feb154768e91419cbeb56c47f3b476c
Feugent <55332231+Feugent@users.noreply.github.com>
2020-08-01 14:36:09 +0200

    Add required upgrades description until there is a better solution

--- ZeroWars.sdd/luarules/configs/heroes/heroduck.lua
+++ ZeroWars.sdd/luarules/configs/heroes/heroduck.lua
@@ -12,7 +12,7 @@ local heroduck = {
     path1 = {
         {
             name = "Heavy Grenade",
-            desc = "+25% Damage",
+            desc = "+25% Damage \n Required Upgrades 0",
             params = {
                 {"WeaponDamage", 1, 0.25}
             },
@@ -20,7 +20,7 @@ local heroduck = {
         },
         {
             name = "HESH Ammo",
-            desc = "+25% AOE",
+            desc = "+25% AOE \n Required Upgrades 1",
             params = {
                 {"WeaponAOE", 1, 0.25}
             },
@@ -28,7 +28,7 @@ local heroduck = {
         },
         {
             name = "Impact Upgrade",
-            desc = "+25% Damage",
+            desc = "+25% Damage \n Required Upgrades 4",
             params = {
                 {"WeaponDamage", 1, 0.25}
             },
@@ -36,7 +36,7 @@ local heroduck = {
         },
         {
             name = "Cluster Grenade",
-            desc = "+25% AOE",
+            desc = "+25% AOE \n Required Upgrades 6",
             params = {
                 {"WeaponAOE", 1, 0.25}
             },
@@ -46,7 +46,7 @@ local heroduck = {
     path2 = {
         {
             name = "Extended Mag",
-            desc = "+25% Fire Rate",
+            desc = "+25% Fire Rate \n Required Upgrades 0",
             params = {
                 {"WeaponReload", 1, 0.25}
             },
@@ -54,7 +54,7 @@ local heroduck = {
         },
         {
             name = "Autoloader",
-            desc = "+25% Fire Rate",
+            desc = "+25% Fire Rate \n Required Upgrades 1",
             params = {
                 {"WeaponReload", 1, 0.25}
             },
@@ -62,7 +62,7 @@ local heroduck = {
         },
         {
             name = "Double Salvo",
-            desc = "Two Salvos\n-50% Fire Rate",
+            desc = "Two Salvos\n-50% Fire Rate \n Required Upgrades 4",
             params = {
                 {"WeaponBurst", 1, 1},
                 {"WeaponReload", 1, -0.5}
@@ -71,7 +71,7 @@ local heroduck = {
         },
         {
             name = "Liquid Cooling",
-            desc = "+25% Fire Rate",
+            desc = "+25% Fire Rate \n Required Upgrades 6",
             params = {
                 {"WeaponReload", 1, 0.25}
             },
@@ -81,7 +81,7 @@ local heroduck = {
     path3 = {
         {
             name = "Light Armour",
-            desc = "+800 HP",
+            desc = "+800 HP \n Required Upgrades 0",
             params = {
                 {"Armour", "800"}
             },
@@ -89,7 +89,7 @@ local heroduck = {
         },
         {
             name = "Medium Armour",
-            desc = "+800 HP",
+            desc = "+800 HP \n Required Upgrades 1",
             params = {
                 {"Armour", "800"}
             },
@@ -97,7 +97,7 @@ local heroduck = {
         },
         {
             name = "Heavy Armour",
-            desc = "+800 HP",
+            desc = "+800 HP \n Required Upgrades 4",
             params = {
                 {"Armour", "800"}
             },
@@ -105,7 +105,7 @@ local heroduck = {
         },
         {
             name = "Ultimate Armour",
-            desc = "+800 HP",
+            desc = "+800 HP \n Required Upgrades 6",
             params = {
                 {"Armour", "800"}
             },
@@ -115,7 +115,7 @@ local heroduck = {
     path4 = {
         {
             name = "E.M.P Blast",
-            desc = "Unlock E.M.P Blast",
+            desc = "Unlock E.M.P Blast \n Required Upgrades 6",
             params = {
                 {"EnableCommand", CMD.MANUALFIRE, true}
             },
@@ -123,7 +123,7 @@ local heroduck = {
         },
         {
             name = "E.M.P Overdrive",
-            desc = "+75% Paralyze Time",
+            desc = "+75% Paralyze Time \n Required Upgrades 8",
             params = {
                 {"WeaponParalyzeTime", 2, 0.75}
             },
@@ -131,7 +131,7 @@ local heroduck = {
         },
         {
             name = "E.M.P Overclock",
-            desc = "+75% Paralyze Time",
+            desc = "+75% Paralyze Time \n Required Upgrades 10",
             params = {
                 {"WeaponParalyzeTime", 2, 0.75}
             },
@@ -139,7 +139,7 @@ local heroduck = {
         },
         {
             name = "E.M.P Storm",
-            desc = "+75% Paralyze Time",
+            desc = "+75% Paralyze Time \n Required Upgrades 12",
             params = {
                 {"WeaponParalyzeTime", 2, 0.75}
             },
